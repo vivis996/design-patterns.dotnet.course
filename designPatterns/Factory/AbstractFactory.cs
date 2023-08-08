c95011e49bb876b086b555982c5c80734c4c1e56
Daniel Viveros <dviveros90@gmail.com>
2023-08-08 18:06:52 -0400

    Abstract Factory and OCP

--- designPatterns/Factory/AbstractFactory.cs
+++ designPatterns/Factory/AbstractFactory.cs
@@ -46,30 +46,71 @@ internal class CoffeeFactory : IHotDrinkFactory
 
 public class HotDrinkMachine
 {
-    public enum AvailableDrink
-    {
-        Coffee,
-        Tea,
-    }
+    //public enum AvailableDrink
+    //{
+    //    Coffee,
+    //    Tea,
+    //}
 
-    private Dictionary<AvailableDrink, IHotDrinkFactory> factories = new();
+    //private Dictionary<AvailableDrink, IHotDrinkFactory> factories = new();
+
+    //public HotDrinkMachine()
+    //{
+    //    foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+    //    {
+    //        IHotDrinkFactory factory = (IHotDrinkFactory)Activator
+    //                                        .CreateInstance(Type
+    //                                                        .GetType("designPatterns.Factory." +
+    //                                                                 Enum.GetName(typeof(AvailableDrink), drink) +
+    //                                                                 "Factory"));
+    //        factories.Add(drink, factory);
+    //    }
+    //}
+
+    //public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+    //{
+    //    return factories[drink].Prepare(amount);
+    //}
+    private List<Tuple<string, IHotDrinkFactory>> factories = new();
 
     public HotDrinkMachine()
     {
-        foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+        foreach (var t in typeof(HotDrinkMachine).Assembly.GetTypes())
         {
-            IHotDrinkFactory factory = (IHotDrinkFactory)Activator
-                                            .CreateInstance(Type
-                                                            .GetType("designPatterns.Factory." +
-                                                                     Enum.GetName(typeof(AvailableDrink), drink) +
-                                                                     "Factory"));
-            factories.Add(drink, factory);
+            if (typeof(IHotDrinkFactory).IsAssignableFrom(t) && !t.IsInterface)
+            {
+                factories.Add(Tuple.Create(t.Name.Replace("Factory", string.Empty), Activator.CreateInstance(t) as IHotDrinkFactory));
+            }
         }
     }
 
-    public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+    public IHotDrink MakeDrink()
     {
-        return factories[drink].Prepare(amount);
+        Console.WriteLine("Available drinks:");
+        for (int i = 0; i < factories.Count; i++)
+        {
+            var tuple = factories[i];
+            Console.WriteLine($"{i}: {tuple.Item1}");
+        }
+        while (true)
+        {
+            Console.WriteLine("Select an available drink.");
+            string s;
+            if ((s = Console.ReadLine()) != null &&
+                int.TryParse(s, out int i) &&
+                i >= 0 &&
+                i < factories.Count)
+            {
+                Console.WriteLine("Specify amount:");
+                if ((s = Console.ReadLine()) != null &&
+                    int.TryParse(s, out int amount) &&
+                    amount > 0)
+                {
+                    return factories[i].Item2.Prepare(amount);
+                }
+            }
+            Console.WriteLine($"Incorrect input, try again!{Environment.NewLine}");
+        }
     }
 }
 
@@ -78,7 +119,8 @@ public class AbstractFactory : IRun
     public void Run()
     {
         var machine = new HotDrinkMachine();
-        var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 100);
+        var drink = machine.MakeDrink();
+        //var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 100);
         drink.Consume();
     }
 }
