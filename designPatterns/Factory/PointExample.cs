c414d31b32d0d25d1d78be10f7844e71d19e60b2
Daniel Viveros <dviveros90@gmail.com>
2023-08-08 17:11:33 -0400

    Inner Factory

--- designPatterns/Factory/PointExample.cs
+++ designPatterns/Factory/PointExample.cs
@@ -2,22 +2,9 @@
 
 public enum CoordinateSystem
 {
+    None,
     Cartesian,
     Polar,
-}
-
-public static class PointFactory
-{
-    // factory method
-    public static Point NewCartesianPoint(double x, double y)
-    {
-        return new Point(x, y, CoordinateSystem.Cartesian);
-    }
-
-    public static Point NewPolarPoint(double rho, double theta)
-    {
-        return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta), CoordinateSystem.Polar);
-    }
 }
 
 public class Point
@@ -25,7 +12,7 @@ public class Point
     private double x, y;
     private CoordinateSystem system;
 
-    public Point(double x, double y, CoordinateSystem system)
+    private Point(double x, double y, CoordinateSystem system)
     {
         this.x = x;
         this.y = y;
@@ -36,13 +23,31 @@ public class Point
     {
         return $"{nameof(system)}: {system}, {nameof(x)}: {x}, {nameof(y)}: {y}";
     }
+
+    public static Point Origin => new Point(0, 0, CoordinateSystem.None);
+
+    public static Point Origin2 => new Point(0, 0, CoordinateSystem.None);
+
+    public static class Factory
+    {
+        // factory method
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y, CoordinateSystem.Cartesian);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta), CoordinateSystem.Polar);
+        }
+    }
 }
 
 public class PointExample : IRun
 {
     public void Run()
     {
-        var point = PointFactory.NewPolarPoint(1.0, Math.PI / 2);
+        var point = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
         Console.WriteLine(point);
     }
 }
