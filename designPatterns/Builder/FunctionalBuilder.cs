7f19c5955a12c61598002bcb9f4ff131f13e7b73
Daniel Viveros <dviveros90@gmail.com>
2023-08-07 20:24:10 -0400

    Functional Builder

---
+++ designPatterns/Builder/FunctionalBuilder.cs
@@ -0,0 +1,72 @@
+﻿namespace designPatterns.Builder.FunctionalBuilder;
+
+public class Person
+{
+    public string Name, Position;
+}
+
+public abstract class FuncitonalBuilder<TSubject, TSelf>
+                where TSelf : FuncitonalBuilder<TSubject, TSelf>
+                where TSubject : new()
+{
+    private readonly List<Func<Person, Person>> actions
+                        = new();
+
+    public TSelf Do(Action<Person> action) => AddAction(action);
+
+    private TSelf AddAction(Action<Person> action)
+    {
+        actions.Add(p =>
+        {
+            action(p);
+            return p;
+        });
+
+        return (TSelf)this;
+    }
+    public Person Build() => actions.Aggregate(new Person(), (p, f) => f(p));
+}
+
+public sealed class PersonBuilder : FuncitonalBuilder<Person, PersonBuilder>
+{
+    public PersonBuilder Called(string name) => Do(p => p.Name = name);
+}
+
+//public sealed class PersonBuilder
+//{
+//    private readonly List<Func<Person, Person>> actions
+//                        = new List<Func<Person, Person>>();
+
+//    public PersonBuilder Called(string name) => Do(p => p.Name = name);
+
+//    public PersonBuilder Do(Action<Person> action) => AddAction(action);
+
+//    private PersonBuilder AddAction(Action<Person> action)
+//    {
+//        actions.Add(p => {
+//            action(p);
+//            return p;
+//        });
+
+//        return this;
+//    }
+
+//    public Person Build() => actions.Aggregate(new Person(), (p, f) => f(p));
+//}
+
+public static class PersonBuilderExtensions
+{
+    public static PersonBuilder WorksAs(this PersonBuilder builder, string position) => builder.Do(p => p.Position = position);
+}
+
+public class FunctionalBuilder : IRun
+{
+    public void Run()
+    {
+        var person = new PersonBuilder()
+                .Called("Daniel")
+                .WorksAs("Developer")
+                .Build();
+    }
+}
+
--- designPatterns/Program.cs
+++ designPatterns/Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     public static void Main()
     {
-        IRun p = new Builder.StepwiseBuilder();
+        IRun p = new Builder.FunctionalBuilder.FunctionalBuilder();
         p.Run();
     }
 }
