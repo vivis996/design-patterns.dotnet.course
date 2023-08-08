406edfb1bd19825004f6df142011c7fe1fce79ee
Daniel Viveros <dviveros90@gmail.com>
2023-08-08 17:35:04 -0400

    Abstract Factory

---
+++ designPatterns/Factory/AbstractFactory.cs
@@ -0,0 +1,84 @@
+﻿namespace designPatterns.Factory;
+
+public interface IHotDrink
+{
+    void Consume();
+}
+
+internal class Tea : IHotDrink
+{
+    public void Consume()
+    {
+        Console.WriteLine("This tea is nice but I'd prefer it with milk.");
+    }
+}
+
+internal class Coffee : IHotDrink
+{
+    public void Consume()
+    {
+        Console.WriteLine("This coffee is sensational!");
+    }
+}
+
+public interface IHotDrinkFactory
+{
+    IHotDrink Prepare(int amount);
+}
+
+internal class TeaFactory : IHotDrinkFactory
+{
+    public IHotDrink Prepare(int amount)
+    {
+        Console.WriteLine($"Put in a tea bag, boil water, pour {amount} ml, add lemon, enjoy");
+        return new Tea();
+    }
+}
+
+internal class CoffeeFactory : IHotDrinkFactory
+{
+    public IHotDrink Prepare(int amount)
+    {
+        Console.WriteLine($"Grind some beans, boil water, pour {amount} ml, add cream and sugar, enjoy");
+        return new Coffee();
+    }
+}
+
+public class HotDrinkMachine
+{
+    public enum AvailableDrink
+    {
+        Coffee,
+        Tea,
+    }
+
+    private Dictionary<AvailableDrink, IHotDrinkFactory> factories = new();
+
+    public HotDrinkMachine()
+    {
+        foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+        {
+            IHotDrinkFactory factory = (IHotDrinkFactory)Activator
+                                            .CreateInstance(Type
+                                                            .GetType("designPatterns.Factory." +
+                                                                     Enum.GetName(typeof(AvailableDrink), drink) +
+                                                                     "Factory"));
+            factories.Add(drink, factory);
+        }
+    }
+
+    public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+    {
+        return factories[drink].Prepare(amount);
+    }
+}
+
+public class AbstractFactory : IRun
+{
+    public void Run()
+    {
+        var machine = new HotDrinkMachine();
+        var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 100);
+        drink.Consume();
+    }
+}
--- designPatterns/Program.cs
+++ designPatterns/Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     public static void Main()
     {
-        IRun p = new Factory.ObjectTracking();
+        IRun p = new Factory.AbstractFactory();
         p.Run();
     }
 }
